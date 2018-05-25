34d1527bb5e258f79bfd0cecd5341f2b5f8d144c
user140257 <user140257@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-05-25 17:31:01 -0400

    Aula sobre comentários, variáveis e tipos

---
+++ _Udemy/Swift2 - Tipos.playground/Contents.swift
@@ -0,0 +1,51 @@
+
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//Int, Float, Double, Character, String, Bool
+
+//Boll
+var isFirstTime: Bool = true
+var  likesFruits = true
+
+//Int
+var age: Int = 30
+var newAge = 43
+var temperature: Int  = -12
+
+//UInt - //Usigned Int
+var age2: UInt = 39
+
+//Float -> 32Bit
+var dolar: Float = 3.5
+
+//Double -> 65Bit
+let crazyNumber = 1000064.06459
+
+//Character
+var gender: Character = "M"
+var enter: Character = "\n"
+var aspas: Character = "\""
+
+//String
+var newGender = "M"
+let studentName = "\"Dr.\" Paulo Henrique Bastos"
+print(studentName)
+let firtName  =  "Daniel"
+let lastName = "Lima"
+//let fullName = firtName + " " + lastName
+
+let fullName =  "\(firtName) \(lastName) - Idade: \(age) anos."
+print("A variável fullName tem: \(fullName.count) caracteres")
+print(fullName)
+let text = """
+Olá, espero que esteja gostado do curso.
+Ainda tem muito mais pela frente
+"""
+print(text) 
+
+
+
